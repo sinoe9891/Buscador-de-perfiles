262086a696dde3d928a8c49244d009957c7e71bd
sinoe9891 <danny_velasquez_cadenas@hotmail.es>
2017-10-09 13:43:38 -0600

    Primer Commit

---
+++ includes/header.php
@@ -0,0 +1,28 @@
+<head>
+    <meta name="viewport" content="width=device-width, user-scalable=no initial-scale=1.0 maximun-scale=1.0">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <title>Perfiles de Graduados | Universidad Zamorano</title>
+    <link rel="icon" href="./img/favicon.png" type="image/x-icon" />
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="css/materialize/materialize.min.css">
+    <style type="text/css">
+        body {
+          display: flex;
+          min-height: 100vh;
+          flex-direction: column;
+        }
+
+        main {
+          flex: 1 0 auto;
+        }
+
+        footer.page-footer {
+          background-color: #931919;
+        }
+
+        nav {
+          background-color: #008341;
+        }
+    </style>
+</head>
+
